e5b01411cf105946a1802a4d9a754b55f5e8a7d6
joajimenez <jimenez.donaldson@gmail.com>
2022-01-26 17:51:20 -0400

    fixed title margin and padding.

--- scipt.js
+++ scipt.js
@@ -1,5 +1,10 @@
 const calculateBtn = document.getElementById('calculate-btn');
 
+let formatter = new Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+});
+
 calculateBtn.addEventListener('click', function () {
   const billAmount = document.getElementById('bill-amount-input').value;
   const percentageTip = document.getElementById('percentage-tip-input').value;
@@ -8,11 +13,11 @@ calculateBtn.addEventListener('click', function () {
 
   const tipAmountDisplay = (document.getElementById(
     'tip-amount-display'
-  ).innerHTML = `$${tipAmount.toLocaleString()}`);
+  ).innerHTML = `$${tipAmount.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')}`);
 
   let total = parseFloat(billAmount) + parseFloat(tipAmount);
 
   const totalDisplay = (document.getElementById(
     'total-amount-display'
-  ).innerHTML = `$${total.toLocaleString()}`);
+  ).innerHTML = `$${total.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')}`);
 });
